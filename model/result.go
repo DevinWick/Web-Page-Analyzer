a9b12d538dbd0a7dafae5578e3ef5fc76076840b
Devin Wickramasinghe <Devin.Wickramsinghe@dialog.lk>
2025-06-10 19:25:03 +0530

    add result model

---
+++ model/result.go
@@ -0,0 +1,20 @@
+package model
+
+type AnalysisResult struct {
+	StatusCode   int
+	URL          string
+	Title        string
+	HTMLVersion  string
+	Headings     map[string]int
+	Links        LinkAnalysis
+	HasLoginForm bool
+	Error        string
+}
+
+type LinkAnalysis struct {
+	InternalLinks         int
+	ExternalLinks         int
+	InaccessibleLinks     int
+	TotalLinks            int
+	InaccessibleLinksList []string
+}
